ab8598ff9ebaf3dc39a7e7ae95dec2ee334b2d38
Jazbo Gross <jazbogross@gmail.com>
2026-02-03 17:22:54 +0000

    update function

--- apps/functions/src/github-create-repo.ts
+++ apps/functions/src/github-create-repo.ts
@@ -37,7 +37,7 @@ export const handler: Handler = async (event) => {
       };
     }
 
-    const response = await fetch(`${GITHUB_API}/repos/${templateOwner}/${templateRepo}/templates/default-theme`, {
+    const response = await fetch(`${GITHUB_API}/repos/${templateOwner}/${templateRepo}/generate`, {
       method: "POST",
       headers: {
         Authorization: `Bearer ${token}`,
